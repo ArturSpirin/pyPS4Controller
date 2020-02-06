b30378f907611a9142385b15a724d6fea6c1f8a7
ArturSpirin <a.spirin@hotmail.com>
2020-02-05 20:45:18 -0800

    Added support for more buttons
    If connecting without ds4drv, now able to get events for:
    - L3 & R3 clicks/releases
    - Playstation button
    - left/right arrow buttons

--- pyPS4Controller/controller.py
+++ pyPS4Controller/controller.py
@@ -88,7 +88,16 @@ class Actions:
     def on_L3_right(self, value):
         pass
 
+    def on_L3_at_rest(self):
+        """R3 joystick is at rest after the joystick was moved and let go off"""
+        pass
+
+    def on_L3_press(self):
+        """R3 joystick is clicked"""
+        pass
+
     def on_L3_release(self):
+        """R3 joystick is released after the click"""
         pass
 
     def on_R3_up(self, value):
@@ -103,7 +112,16 @@ class Actions:
     def on_R3_right(self, value):
         pass
 
+    def on_R3_at_rest(self):
+        """R3 joystick is at rest after the joystick was moved and let go off"""
+        pass
+
+    def on_R3_press(self):
+        """R3 joystick is clicked. This event is only detected when connecting without ds4drv"""
+        pass
+
     def on_R3_release(self):
+        """R3 joystick is released after the click. This event is only detected when connecting without ds4drv"""
         pass
 
     def on_options_press(self):
@@ -112,22 +130,40 @@ class Actions:
     def on_options_release(self):
         pass
 
+    def on_share_press(self):
+        """this event is only detected when connecting without ds4drv"""
+        pass
+
+    def on_share_release(self):
+        """this event is only detected when connecting without ds4drv"""
+        pass
+
+    def on_playstation_button_press(self):
+        """this event is only detected when connecting without ds4drv"""
+        pass
+
+    def on_playstation_button_release(self):
+        """this event is only detected when connecting without ds4drv"""
+        pass
+
 
 class Controller(Actions):
 
     EVENT_SIZE = struct.calcsize("LhBB")
 
-    def __init__(self, interface, via_bluetoothctl=False):
+    def __init__(self, interface, connecting_using_ds4drv=True):
         """
         Initiate controller instance that is capable of listening to all events on specified input interface
         :param interface: STRING aka /dev/input/js0 or any other PS4 Duelshock controller interface.
-        :param via_bluetoothctl: BOOLEAN. If you are using ds4drv leave it set to False.
-                                          If connecting your device using bluetoothctl cli set this to True.
+        :param connecting_using_ds4drv: BOOLEAN. If you are connecting your controller using ds4drv, then leave it set
+                                                 to True. Otherwise if you are connecting directly via directly via
+                                                 bluetooth/bluetoothctl, set it to False otherwise the controller
+                                                 button mapping will be off.
         """
         Actions.__init__(self)
         self.stop = False
         self.interface = interface
-        self.via_bluetoothctl = via_bluetoothctl
+        self.connecting_using_ds4drv = connecting_using_ds4drv
         self.debug = False  # If you want to see raw event stream, set this to True.
         self.black_listed_buttons = [6, 7, 8, 11, 12, 13]  # set a list of blocked buttons
 
@@ -158,8 +194,8 @@ class Controller(Actions):
     def __event(self, button_id, button_type, value):
 
         # L joystick group #
-        def L3_event():
-            return button_type == 2 and button_id in [1, 0]
+        def L3_event():  # L3 has the same mapping on ds4drv as it does when connecting  to bluetooth directly
+            return button_type == 2 and button_id in [1, 0, 11]
 
         def L3_at_rest():
             return button_id in [1, 0] and value == 0
@@ -176,36 +212,56 @@ class Controller(Actions):
         def L3_right():
             return button_id == 0 and value > 0
 
+        def L3_pressed():
+            if not self.connecting_using_ds4drv:
+                return button_id == 11 and button_type == 1 and value == 1
+            return False  # cant identify this event when connected through ds4drv
+
+        def L3_released():
+            if not self.connecting_using_ds4drv:
+                return button_id == 11 and button_type == 1 and value == 0
+            return False  # cant identify this event when connected through ds4drv
+
         # R joystick group #
         def R3_event():
-            if self.via_bluetoothctl:
-                return button_type == 2 and button_id in [4, 3]
+            if not self.connecting_using_ds4drv:
+                return button_type == 2 and button_id in [4, 3, 12]
             return button_type == 2 and button_id in [5, 2]
 
         def R3_at_rest():
-            if self.via_bluetoothctl:
+            if not self.connecting_using_ds4drv:
                 return button_id in [4, 3] and value == 0
             return button_id in [2, 5] and value == 0
 
         def R3_up():
-            if self.via_bluetoothctl:
+            if not self.connecting_using_ds4drv:
                 return button_id == 4 and value < 0
             return button_id == 5 and value < 0
 
         def R3_down():
-            if self.via_bluetoothctl:
+            if not self.connecting_using_ds4drv:
                 return button_id == 4 and value > 0
             return button_id == 5 and value > 0
 
         def R3_left():
-            if self.via_bluetoothctl:
+            if not self.connecting_using_ds4drv:
                 return button_id == 3 and value < 0
             return button_id == 2 and value < 0
 
         def R3_right():
-            if self.via_bluetoothctl:
+            if not self.connecting_using_ds4drv:
                 return button_id == 3 and value > 0
             return button_id == 2 and value > 0
+
+        def R3_pressed():
+            if not self.connecting_using_ds4drv:
+                return button_id == 12 and button_type == 1 and value == 1
+            return False  # cant identify this event when connected through ds4drv
+
+        def R3_released():
+            if not self.connecting_using_ds4drv:
+                return button_id == 12 and button_type == 1 and value == 0
+            return False  # cant identify this event when connected through ds4drv
 
         # Square / Triangle / Circle / X Button group #
         def circle_pressed():
@@ -238,6 +294,16 @@ class Controller(Actions):
         def options_released():
             return button_id == 9 and button_type == 1 and value == 0
 
+        def share_pressed():
+            if not self.connecting_using_ds4drv:
+                return button_id == 8 and button_type == 1 and value == 1
+            return False  # cant identify this event when connected through ds4drv
+
+        def share_released():
+            if not self.connecting_using_ds4drv:
+                return button_id == 8 and button_type == 1 and value == 0
+            return False  # cant identify this event when connected through ds4drv
+
         # N1 group #
         def L1_pressed():
             return button_id == 4 and button_type == 1 and value == 1
@@ -253,62 +319,76 @@ class Controller(Actions):
 
         # N2 group #
         def L2_pressed():
-            if self.via_bluetoothctl:
+            if not self.connecting_using_ds4drv:
                 return button_id == 2 and button_type == 2 and (32767 >= value >= -32766)
             return button_id == 3 and button_type == 2 and (32767 >= value >= -32766)
 
         def L2_released():
-            if self.via_bluetoothctl:
+            if not self.connecting_using_ds4drv:
                 return button_id == 2 and button_type == 2 and value == -32767
             return button_id == 3 and button_type == 2 and value == -32767
 
         def R2_pressed():
-            if self.via_bluetoothctl:
+            if not self.connecting_using_ds4drv:
                 return button_id == 5 and button_type == 2 and (32767 >= value >= -32766)
             return button_id == 4 and button_type == 2 and (32767 >= value >= -32766)
 
         def R2_released():
-            if self.via_bluetoothctl:
+            if not self.connecting_using_ds4drv:
                 return button_id == 5 and button_type == 2 and value == -32767
             return button_id == 4 and button_type == 2 and value == -32767
 
         # up / down arrows #
-        def up_arrow_press():
-            if self.via_bluetoothctl:
+        def up_arrow_pressed():
+            if not self.connecting_using_ds4drv:
                 return button_id == 7 and button_type == 2 and value == -32767
             return button_id == 10 and button_type == 2 and value == -32767
 
-        def down_arrow_press():
-            if self.via_bluetoothctl:
+        def down_arrow_pressed():
+            if not self.connecting_using_ds4drv:
                 return button_id == 7 and button_type == 2 and value == 32767
             return button_id == 10 and button_type == 2 and value == 32767
 
-        def up_down_arrow_release():
+        def up_down_arrow_released():
             # arrow buttons on release are not distinguishable and if you think about it,
             # they are following same principle as the joystick buttons which only have 1
             # state at rest which is shared between left/ right / up /down inputs
-            if self.via_bluetoothctl:
+            if not self.connecting_using_ds4drv:
                 return button_id == 7 and button_type == 2 and value == 0
             return button_id == 10 and button_type == 2 and value == 0
 
         # left / right arrows #
-        def left_arrow_press():
+        def left_arrow_pressed():
+            if not self.connecting_using_ds4drv:
+                return button_id == 6 and button_type == 2 and value == -32767
             return button_id == 9 and button_type == 2 and value == -32767
 
-        def right_arrow_press():
+        def right_arrow_pressed():
+            if not self.connecting_using_ds4drv:
+                return button_id == 6 and button_type == 2 and value == 32767
             return button_id == 9 and button_type == 2 and value == 32767
 
-        def left_right_arrow_release():
+        def left_right_arrow_released():
             # arrow buttons on release are not distinguishable and if you think about it,
             # they are following same principle as the joystick buttons which only have 1
             # state at rest which is shared between left/ right / up /down inputs
-            if self.via_bluetoothctl:
+            if not self.connecting_using_ds4drv:
                 return button_id == 6 and button_type == 2 and value == 0
             return button_id == 9 and button_type == 2 and value == 0
 
+        def playstation_button_pressed():
+            if not self.connecting_using_ds4drv:
+                return button_id == 10 and button_type == 1 and value == 1
+            return False  # cant identify this event when connected through ds4drv
+
+        def playstation_button_released():
+            if not self.connecting_using_ds4drv:
+                return button_id == 10 and button_type == 1 and value == 0
+            return False  # cant identify this event when connected through ds4drv
+
         if R3_event():
             if R3_at_rest():
-                self.on_R3_release()
+                self.on_R3_at_rest()
             elif R3_right():
                 self.on_R3_right(value)
             elif R3_left():
@@ -317,9 +397,13 @@ class Controller(Actions):
                 self.on_R3_up(value)
             elif R3_down():
                 self.on_R3_down(value)
+            elif R3_pressed():
+                self.on_R3_press()
+            elif R3_released():
+                self.on_R3_release()
         elif L3_event():
             if L3_at_rest():
-                self.on_L3_release()
+                self.on_L3_at_rest()
             elif L3_up():
                 self.on_L3_up(value)
             elif L3_down():
@@ -328,6 +412,10 @@ class Controller(Actions):
                 self.on_L3_left(value)
             elif L3_right():
                 self.on_L3_right(value)
+            elif L3_pressed():
+                self.on_L3_press()
+            elif L3_released():
+                self.on_L3_release()
         elif circle_pressed():
             self.on_circle_press()
         elif circle_released():
@@ -364,15 +452,23 @@ class Controller(Actions):
             self.on_options_press()
         elif options_released():
             self.on_options_release()
-        elif left_right_arrow_release():
+        elif left_right_arrow_released():
             self.on_left_right_arrow_release()
-        elif up_down_arrow_release():
+        elif up_down_arrow_released():
             self.on_up_down_arrow_release()
-        elif left_arrow_press():
+        elif left_arrow_pressed():
             self.on_left_arrow_press()
-        elif right_arrow_press():
+        elif right_arrow_pressed():
             self.on_right_arrow_press()
-        elif up_arrow_press():
+        elif up_arrow_pressed():
             self.on_up_arrow_press()
-        elif down_arrow_press():
+        elif down_arrow_pressed():
             self.on_down_arrow_press()
+        elif playstation_button_pressed():
+            self.on_playstation_button_press()
+        elif playstation_button_released():
+            self.on_playstation_button_release()
+        elif share_pressed():
+            self.on_share_press()
+        elif share_released():
+            self.on_share_release()
