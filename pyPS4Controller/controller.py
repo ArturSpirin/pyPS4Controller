503ad97c45275eb9513c8f36a07421a21a3b0db0
Ptit-Biscuit <vincent.brebion@epsi.fr>
2020-10-06 15:02:28 +0200

    feature: Callback for special inputs sequences

--- pyPS4Controller/controller.py
+++ pyPS4Controller/controller.py
@@ -397,8 +397,9 @@ class Controller(Actions):
         self.event_definition = event_definition if event_definition else Event
         self.event_format = event_format if event_format else "LhBB"
         self.event_size = struct.calcsize(self.event_format)
+        self.event_history = []
 
-    def listen(self, timeout=30, on_connect=None, on_disconnect=None):
+    def listen(self, timeout=30, on_connect=None, on_disconnect=None, on_special_inputs=None):
         """
         Start listening for events on a given self.interface
         :param timeout: INT, seconds. How long you want to wait for the self.interface.
@@ -406,6 +407,9 @@ class Controller(Actions):
                         If self.interface does not become available in N seconds, the script will exit with exit code 1.
         :param on_connect: function object, allows to register a call back when connection is established
         :param on_disconnect: function object, allows to register a call back when connection is lost
+        :param on_special_inputs: list, allows to register a call back on specific input sequence.
+                                  e.g [{"inputs": ['up', 'up', 'down', 'down', 'left', 'right, 'left', 'right, 'start',
+                                  'options'], "callback": () -> None)}]
         :return: None
         """
         def on_disconnect_callback():
@@ -437,16 +441,28 @@ class Controller(Actions):
                 on_disconnect_callback()
                 exit(1)
 
+        def check_for(sub, full, start_index):
+            return [start for start in range(start_index, len(full) - len(sub) + 1) if
+                    sub == full[start:start + len(sub)]]
+
         wait_for_interface()
         try:
             _file = open(self.interface, "rb")
             event = read_events()
+            if on_special_inputs is None:
+                on_special_inputs = []
+            special_inputs_indexes = [0] * len(on_special_inputs)
             while not self.stop and event:
                 (*tv_sec, value, button_type, button_id) = struct.unpack(self.event_format, event)
                 if self.debug:
                     print("button_id: {} button_type: {} value: {}".format(button_id, button_type, value))
                 if button_id not in self.black_listed_buttons:
                     self.__handle_event(button_id=button_id, button_type=button_type, value=value)
+                for i, special_input in enumerate(on_special_inputs):
+                    check = check_for(special_input["inputs"], self.event_history, special_inputs_indexes[i])
+                    if len(check) != 0:
+                        special_inputs_indexes[i] = check[0] + 1
+                        special_input["callback"]()
                 event = read_events()
         except KeyboardInterrupt:
             print("\nExiting (Ctrl + C)")
@@ -461,6 +477,7 @@ class Controller(Actions):
                                       connecting_using_ds4drv=self.connecting_using_ds4drv)
 
         if event.R3_event():
+            self.event_history.append("right_joystick")
             if event.R3_y_at_rest():
                 self.on_R3_y_at_rest()
             elif event.R3_x_at_rest():
@@ -474,6 +491,7 @@ class Controller(Actions):
             elif event.R3_down():
                 self.on_R3_down(value)
         elif event.L3_event():
+            self.event_history.append("left_joystick")
             if event.L3_y_at_rest():
                 self.on_L3_y_at_rest()
             elif event.L3_x_at_rest():
@@ -487,38 +505,47 @@ class Controller(Actions):
             elif event.L3_right():
                 self.on_L3_right(value)
         elif event.circle_pressed():
+            self.event_history.append("circle")
             self.on_circle_press()
         elif event.circle_released():
             self.on_circle_release()
         elif event.x_pressed():
+            self.event_history.append("x")
             self.on_x_press()
         elif event.x_released():
             self.on_x_release()
         elif event.triangle_pressed():
+            self.event_history.append("triangle")
             self.on_triangle_press()
         elif event.triangle_released():
             self.on_triangle_release()
         elif event.square_pressed():
+            self.event_history.append("square")
             self.on_square_press()
         elif event.square_released():
             self.on_square_release()
         elif event.L1_pressed():
+            self.event_history.append("L1")
             self.on_L1_press()
         elif event.L1_released():
             self.on_L1_release()
         elif event.L2_pressed():
+            self.event_history.append("L2")
             self.on_L2_press(value)
         elif event.L2_released():
             self.on_L2_release()
         elif event.R1_pressed():
+            self.event_history.append("R1")
             self.on_R1_press()
         elif event.R1_released():
             self.on_R1_release()
         elif event.R2_pressed():
+            self.event_history.append("R2")
             self.on_R2_press(value)
         elif event.R2_released():
             self.on_R2_release()
         elif event.options_pressed():
+            self.event_history.append("options")
             self.on_options_press()
         elif event.options_released():
             self.on_options_release()
@@ -527,26 +554,34 @@ class Controller(Actions):
         elif event.up_down_arrow_released():
             self.on_up_down_arrow_release()
         elif event.left_arrow_pressed():
+            self.event_history.append("left")
             self.on_left_arrow_press()
         elif event.right_arrow_pressed():
+            self.event_history.append("right")
             self.on_right_arrow_press()
         elif event.up_arrow_pressed():
+            self.event_history.append("up")
             self.on_up_arrow_press()
         elif event.down_arrow_pressed():
+            self.event_history.append("down")
             self.on_down_arrow_press()
         elif event.playstation_button_pressed():
+            self.event_history.append("ps")
             self.on_playstation_button_press()
         elif event.playstation_button_released():
             self.on_playstation_button_release()
         elif event.share_pressed():
+            self.event_history.append("share")
             self.on_share_press()
         elif event.share_released():
             self.on_share_release()
         elif event.R3_pressed():
+            self.event_history.append("R3")
             self.on_R3_press()
         elif event.R3_released():
             self.on_R3_release()
         elif event.L3_pressed():
+            self.event_history.append("L3")
             self.on_L3_press()
         elif event.L3_released():
             self.on_L3_release()
