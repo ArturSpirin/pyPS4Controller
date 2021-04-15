076c98c753351c4e236c889834a20fff0c568676
Pekka Ahtonen <pekka.ahtonen@iki.fi>
2021-04-15 13:26:01 +0300

    Fix button mappings.

--- pyPS4Controller/event_mapping/Mapping3Bh2b.py
+++ pyPS4Controller/event_mapping/Mapping3Bh2b.py
@@ -25,25 +25,25 @@ class Mapping3Bh2b(DefaultMapping):
 
     # Square / Triangle / Circle / X Button group #
     def circle_pressed(self):
-        return self.button_id == 1 and self.button_type == 1 and self.value == 1
+        return self.button_id == 2 and self.button_type == 1 and self.value == 1        
 
     def circle_released(self):
-        return self.button_id == 1 and self.button_type == 1 and self.value == 0
+        return self.button_id == 2 and self.button_type == 1 and self.value == 0        
 
     def x_pressed(self):
-        return self.button_id == 0 and self.button_type == 1 and self.value == 1
+        return self.button_id == 1 and self.button_type == 1 and self.value == 1
 
     def x_released(self):
-        return self.button_id == 0 and self.button_type == 1 and self.value == 0
+        return self.button_id == 1 and self.button_type == 1 and self.value == 0
 
     def triangle_pressed(self):
-        return self.button_id == 2 and self.button_type == 1 and self.value == 1
-
-    def triangle_released(self):
-        return self.button_id == 2 and self.button_type == 1 and self.value == 0
-
-    def square_pressed(self):
         return self.button_id == 3 and self.button_type == 1 and self.value == 1
 
-    def square_released(self):
+    def triangle_released(self):
         return self.button_id == 3 and self.button_type == 1 and self.value == 0
+
+    def square_pressed(self):
+        return self.button_id == 0 and self.button_type == 1 and self.value == 1
+
+    def square_released(self):
+        return self.button_id == 0 and self.button_type == 1 and self.value == 0
